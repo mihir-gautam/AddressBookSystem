58e6ba71788953f705f88d76d11872740bb820a4
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-01 00:28:53 +0530

    [Mihir] Add Code to create contact added

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -7,6 +7,7 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Program!");
+            CreateContact.Person();
         }
     }
 }
---
+++ CreateContact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace AddressBookSystem
+{
+    class CreateContact
+    {
+        public static void Person()
+        {
+            List<string> data = new List<string>();
+            data.Add("first name");
+            data.Add("last name");
+            data.Add("address");
+            data.Add("city");
+            data.Add("state");
+            data.Add("zip code");
+            data.Add("phone no.");
+            data.Add("email id");
+            for (int i = 0; i < data.Count; i++)
+            {
+                Console.WriteLine("Enter the {0}: ", data.ElementAt(i));
+                string input = Console.ReadLine();
+            }
+        }
+    }
+}
