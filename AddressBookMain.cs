4cd632d467bceb6311758a05ec5e6ecf3be5841d
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-01 00:22:08 +0530

    [Welcome to the address book program]

---
+++ AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Program!");
+        }
+    }
+}
