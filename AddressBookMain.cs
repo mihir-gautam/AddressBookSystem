11c808157a8d5dbdfe4871b839fc0d6129e91203
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-02 15:11:33 +0530

    [Mihir] Add Functionality to add multiple address books added

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
 namespace AddressBookSystem
@@ -17,6 +19,11 @@ namespace AddressBookSystem
             bool exist = true;
             while (exist)
             {
+                string BookName;
+                ArrayList ContactList = new ArrayList();
+                Dictionary<string, ArrayList> Book = new Dictionary<string, ArrayList>();
+                Console.WriteLine("Enter new address book name : ");
+                BookName = Console.ReadLine();
                 Console.WriteLine("Select the option. \n1. Add new contact. \n2. Edit existing contact. \n3. Delete existing contact \n4. Exit.");
                 int option = int.Parse(Console.ReadLine());
                 switch (option)
@@ -24,8 +31,9 @@ namespace AddressBookSystem
                     case 1:
                         {
                             newProgram.book.AddPerson();
+                            Book.Add(BookName, ContactList);
                             Console.WriteLine("Contact added!");
-                            Console.WriteLine("New count of contacts in address book : " + newProgram.book.ContactList.Count);
+                            Console.WriteLine("New count of contacts in address book : " + Book.Count);
                             break;
                         }
                     case 2:
@@ -38,7 +46,7 @@ namespace AddressBookSystem
                         {
                             Console.WriteLine("Enter the first name of that person: ");
                             newProgram.book.DeletePersonDetails();
-                            Console.WriteLine("New count of contacts in address book : " + newProgram.book.ContactList.Count);
+                            Console.WriteLine("New count of contacts in address book : " + Book.Count);
                             break;
                         }
                     case 4:
