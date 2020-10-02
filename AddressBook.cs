1c861347d987ac95d6c44c4cf8230234850b8484
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-02 12:50:03 +0530

    [Mihir] Add Code to edit details of existing contact added

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,41 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AddressBookSystem
 {
     class AddressBook
     {
-        public static void AddPerson()
+
+        List<Contact> Person = new List<Contact>();
+        public void AddPerson()
         {
             Contact contact = new Contact();
-
             Console.Write("Enter First Name: ");
             contact.FirstName = Console.ReadLine();
-
             Console.Write("Enter Last Name: ");
             contact.LastName = Console.ReadLine();
-
-            Console.Write("Enter Address ");
+            Console.Write("Enter Address : ");
             contact.Address = Console.ReadLine();
-
             Console.Write("Enter City : ");
             contact.City = Console.ReadLine();
-            
             Console.Write("Enter State : ");
             contact.State = Console.ReadLine();
-            
             Console.Write("Enter Zip Code : ");
             contact.ZipCode = Console.ReadLine();
-            
             Console.Write("Enter Phone Number : ");
             contact.Phone = Console.ReadLine();
-            
             Console.Write("Enter Email ID : ");
             contact.Email = Console.ReadLine();
 
-            List<Contact> Person = new List<Contact>();
             Person.Add(contact);
+        }
+        public Contact FindPerson(string firstName)
+        {
+            Contact toFind = Person.Find((person) => person.FirstName == firstName);
+            return toFind;
+        }
+        public void EditPersonDetails()
+        {
+            string firstName = Console.ReadLine();
+            Contact editedContact = FindPerson(firstName);
+            if (editedContact == null)
+            {
+                Console.WriteLine("Address for {0} count not be found.", firstName);
+            }
+            else
+            {
+                Console.WriteLine("Exisitng address for {0} found, please update the details.", firstName);
+                Console.WriteLine("New Last Name");
+                editedContact.LastName = Console.ReadLine();
+                Console.WriteLine("New Address");
+                editedContact.Address = Console.ReadLine();
+                Console.WriteLine("New City");
+                editedContact.City = Console.ReadLine();
+                Console.WriteLine("New State");
+                editedContact.State = Console.ReadLine();
+                Console.WriteLine("New Zip code");
+                editedContact.ZipCode = Console.ReadLine();
+                Console.WriteLine("New Phone Number");
+                editedContact.Phone = Console.ReadLine();
+                Console.WriteLine("New Email");
+                editedContact.Email = Console.ReadLine();
+                Console.WriteLine("Contact details updated successfully!");
+            }
         }
     }
 }
--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -5,11 +5,41 @@ namespace AddressBookSystem
 {
     class AddressBookMain
     {
+        AddressBook book;
+        public AddressBookMain()
+        {
+            book = new AddressBook();
+        }
         static void Main(string[] args)
         {
+            AddressBookMain newProgram = new AddressBookMain();
             Console.WriteLine("Welcome to the Address Book Program!");
-            Contact.CreateContact();
-            AddressBook.AddPerson();
+            bool exist = true;
+            while (exist)
+            {
+                Console.WriteLine("Select the option. \n1. Add new contact. \n2. Edit existing contact. \n3. Exit.");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {
+                            newProgram.book.AddPerson();
+                            Console.WriteLine("Contact added!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter the first name of that person: ");
+                            newProgram.book.EditPersonDetails();
+                            break;
+                        }
+                    case 3:
+                        {
+                            exist = false;
+                            break;
+                        }
+                }
+            }
         }
     }
 }
